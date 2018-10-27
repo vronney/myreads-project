da2944add044baa470151eff1a63c1432f158a83
Ronney Vargas <ronneyvargas@gmail.com>
2018-10-27 13:33:15 -0500

    Added project from Udacity workspace to GitHub

--- src/App.js
+++ src/App.js
@@ -1,28 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import BookList from './BookList'
+import Search from './Search'
 
-class App extends Component {
+class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    this.fetchBooks();
+  }
+  fetchBooks() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books
+      })
+    });
+  }
+
+  getShelfBooks(shelfName) {
+    return this.state.books.filter((b) => b.shelf === shelfName)
+  }
+
+  changeShelf = (newBook, newShelf) => {
+    BooksAPI.update(newBook, newShelf).then(response => {
+      newBook.shelf = newShelf;
+
+      var updatedBooks = this.state.books.filter(book => book.id !== newBook.id)
+
+      // add book to array and set new state
+      updatedBooks.push(newBook);
+      this.setState({
+        books: updatedBooks
+      })
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const {
+      books
+    } = this.state
+
+    return ( <
+      div className = "app" >
+      <
+      Route path = "/search"
+      render = {
+        ({
+          history
+        }) => ( <
+          Search books = {
+            books
+          }
+          changeShelf = {
+            this.changeShelf
+          }
+          />
+        )
+      }
+      /> <
+      Route exact path = "/"
+      render = {
+        () => ( <
+          div className = "list-books" >
+          <
+          div className = "list-books-title" >
+          <
+          h1 > MyReads < /h1> <
+          /div> <
+          BookList books = {
+            books
+          }
+          changeShelf = {
+            this.changeShelf
+          }
+          /> <
+          div className = "open-search" >
+          <
+          Link to = "/search" > Search < /Link> <
+          /div> <
+          /div>
+        )
+      }
+      /> <
+      /div>
+    )
   }
 }
 
-export default App;
+export default BooksApp;
